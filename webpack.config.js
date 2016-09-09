de450303d2f4c448d549ac9182e18f106677a356
liRenhao <renhao.li@bjyada.com>
2016-09-09 10:14:52 +0800

    添加react-hot-loader的配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,55 @@
 /**
- * Created by ALIENWARE17 on 2016/9/7.
+ * Author：ALIENWARE17
+ * Create Date：2016/9/7
+ * Modified By：liRenhao
+ * Why & What is modified 添加hmr插件
+ * webpack的配置文件,搭建webpack的环境
  */
-var path = require(`path`);
-var webpack = require(`webpack`);
-var OpenBrowserPlugin = require(`webpack`);
-var HtmlWebpackPlugin = require(`html-webpack-plugin`);
+var path = require("path");
+var webpack = require("webpack");
+var OpenBrowserPlugin = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-		bundle: ['bootstrap-loader', './src/client.js']
-	},
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: '[name].js'
-	},
-	module: {
-		loaders: [
-			{test: /\.js?$/, loader: "babel", include: path.resolve(__dirname, './src')},
-			{test: /\.css$/, loader: "style!css"},
-			{test: /\.s[a,c]ss$/, loader: "style!css!sass"},
-			{
-				test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
-				loader: "url?limit=10000&mimetype=application/font-woff"
-			},
-			{
-				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "url?limit=10000&mimetype=application/octet-stream"
-			},
-			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "url?limit=10000&mimetype=image/svg+xml"
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "测试页",
-		}),
-		// 	new OpenBrowserPlugin({
-		// 		url: "http://localhost:8080"
-		// 	}),
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			"window.jQuery": "jquery"
-		})
-	]
+    entry: {
+        bundle: ["bootstrap-loader", "./src/client.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {test: /\.js?$/, loader: "babel", include: path.resolve(__dirname, "./src")},
+            {test: /\.css$/, loader: "style!css"},
+            {test: /\.s[a,c]ss$/, loader: "style!css!sass"},
+            {
+                test: /\.(woff|woff2)(\?v=\d\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "测试页",
+        }),
+        // 	new OpenBrowserPlugin({
+        // 		url: "http://localhost:8080"
+        // 	}),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
