ce266ac36cbcc3a6cd9357a8e80e9817cc01fdf9
kongli860329@163.com <kongli860329@163.com>
2016-09-12 21:15:48 +0800

    删除多余路由内容

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 /**
  * Author：Yky
  * Create Date：2016/9/12
- * Modified By：Yky
+ * Modified By：Yky KL
  * Why & What is modified  <修改原因描述>
  * 路由文件
  */
@@ -10,13 +10,11 @@ import {Route} from 'react-router'
 import Login from './components/login/Login'
 import AcqOrderId from './components/acquisitionOrderId/AcqOrderId'
 import PayForm from './components/payForm/PayForm'
-import GoodForm from './components/GoodsForm'
 
 export default (
 	<Route>
 		<Route path="/" component={Login}/>
 		<Route path="/acqOrderId" component={AcqOrderId}/>
 		<Route path="/PayForm" component={PayForm}/>
-		<Route path="/GoodForm" component={GoodForm}/>
 	</Route>
 )
