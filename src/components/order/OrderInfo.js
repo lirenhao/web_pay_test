accbb9f86d01c5e3c7369ca7c058346c04f428eb
jpf0318 <pengfei.ji@bjyada.com>
2016-09-12 20:23:24 +0800

    修改订单商品信息组件，添加描述

--- src/components/order/OrderInfo.js
+++ src/components/order/OrderInfo.js
@@ -3,7 +3,7 @@
  * Create Date：2016/9/12
  * Modified By：pengfei
  * Why & What is modified  <修改原因描述>
- * <文件描述>
+ * 订单信息组件封装、商品信息列表其他地方有共用，所以单独封装一个订单商品列表组件
  */
 
 import React from 'react'
@@ -34,3 +34,6 @@ class OrderInfo extends React.Component {
             return (<div role="alert">正在加载订单信息...</div>);
     }
 }
+
+
+export default OrderInfo;
