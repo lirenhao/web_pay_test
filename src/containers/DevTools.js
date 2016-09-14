054fc4e29672e998a0df0f837c9c8e080c29a3e8
liRenhao <renhao.li@bjyada.com>
2016-09-14 15:59:07 +0800

    设置redux-devtools默认隐藏

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -2,7 +2,7 @@
  * Author：liRenhao
  * Create Date：2016/9/10
  * Modified By：liRenhao
- * Why & What is modified  <修改原因描述>
+ * Why & What is modified  devTools工具默认隐藏
  * redux-devtools的插件用于开发过程中方便查看store中state的值
  */
 import React from 'react'
@@ -11,7 +11,8 @@ import DockMonitor from 'redux-devtools-dock-monitor'
 import LogMonitor from 'redux-devtools-log-monitor'
 
 export default createDevTools(
-    <DockMonitor toggleVisibilityKey='ctrl-h'
+    <DockMonitor defaultIsVisible={false}
+                 toggleVisibilityKey='ctrl-h'
                  changePositionKey='ctrl-q'>
         <LogMonitor />
     </DockMonitor>
