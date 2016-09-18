8424a0f24e7803fe7b9ea306e827a9b31737857b
liRenhao <renhao.li@bjyada.com>
2016-09-18 16:06:06 +0800

    orderIdsReducer的处理

---
+++ src/reducers/orderIdsReducer.js
@@ -0,0 +1,20 @@
+/**
+ * Author：liRenhao
+ * Create Date：2016/9/18
+ * Modified By：liRenhao
+ * Why & What is modified  <修改原因描述>
+ * <文件描述>
+ */
+import {createReducer} from "redux-act"
+import {addOrder, remove} from "../actions"
+
+export default createReducer({
+    [addOrder]: (state, orderId) => {
+        return [...state, orderId]
+    },
+    [remove]: (state, orderId) => {
+        return state.filter((value) => {
+            return value !== orderId
+        })
+    }
+}, [])
