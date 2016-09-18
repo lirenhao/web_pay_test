2c22745f8edec3a48cc1cb8cead03d6ee5263bc2
jpf0318 <pengfei.ji@bjyada.com>
2016-09-18 14:54:32 +0800

    添加多个action

--- src/actions.js
+++ src/actions.js
@@ -3,8 +3,15 @@
  * Create Date：2016/9/18
  * Modified By：liRenhao
  * Why & What is modified  <修改原因描述>
+ * Modified By：Pengfei
+ * Why & What is modified  添加多个action
  * 用于描述界面中操作state中数据变化的action
  */
 import {createAction} from "redux-act"
 
-export const userLogin = createAction("user login")
+export const userLogin = createAction("user login");
+export const addUser = createAction("ADD_USER");
+export const addOrder = createAction("ADD_ORDER");
+export const addMarketing=createAction("ADD_MARKETING");
+export const payAuth =createAction("PAY_AUTH");
+export const payCompleted =createAction("PAY_COMPLETED");
