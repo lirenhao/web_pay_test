6c49aa68dece2001f2b529c3b4f5318175f52d32
kongli860329@163.com <kongli860329@163.com>
2016-09-18 15:15:16 +0800

    去掉页面跳转

--- src/containers/Pay.js
+++ src/containers/Pay.js
@@ -46,10 +46,8 @@ const index = 0;
 
 const Pay=(props)=>(
     <PayForm initialValues={{orderId: state.orderIds[index], result: "0"}}
-             onSubmit={(values) => {if(state.user.userType=="USER")
-                 browserHistory.push("/acqOrderId") else browserHistory.push("/Goods"); console.log(values) }}
-             onCancel={(values) => {if(state.user.userType=="USER")
-                 browserHistory.push("/acqOrderId") else browserHistory.push("/Goods"); console.log(values) }}
+             onSubmit={(values) => { console.log(values) }}
+             onCancel={(values) => {console.log(values) }}
              orderIds={state.orderIds}
              order={state.order}
              marketing={state.marketing}
