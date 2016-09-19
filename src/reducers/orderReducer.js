242b3cb1e4198cefdfa6318c2c1d87fc08994460
YuKaiyang <45544205@qq.com>
2016-09-19 17:01:36 +0800

    加入reducer的文件说明， ”添加商品“页面的完整功能， “扫面订单”页面的验证功能，

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -6,7 +6,7 @@
  * Modified By：Yky
  * Why & What is modified  添加文件描述
  * order的reducer:
- * addOrder——将商品信息按照orderId存入state.order（是排序过的，原因还不清楚）
+ * addOrder——将商品信息按照orderId存入state.order
  * 并添加isPayAuth属性，表示该订单是否在支付中，默认值为false
  * payAuth——将orderId对应的isPayAuth标记为true,表示该订单正在支付
  * remove——将orderId对应的订单信息从state.order中移除
