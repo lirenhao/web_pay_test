d307cfc47dc3e994be5f88fc4bc5d31730c0a166
liRenhao <renhao.li@bjyada.com>
2016-09-19 19:13:38 +0800

    PayForm覆盖还原

--- src/components/payForm/PayForm.js
+++ src/components/payForm/PayForm.js
@@ -3,12 +3,14 @@
  * Created by likon on 2016/9/9.
  * Modified By：KL
  * Why & What is modified  <修改原因描述>
+ * Modified By：Pengfei
+ * Why & What is modified  添加订单号和流水号处理和展示样式
  * 添加支付表单组件
  * 添加商品列表组件、优惠信息组件、结算信息组件
  */
 import React from 'react'
 import {Field, reduxForm} from "redux-form"
-import {ButtonGroup,Button, FormControl,MenuItem,Navbar,Form} from 'react-bootstrap'
+import {ButtonGroup, Button, FormControl, Navbar, Form} from 'react-bootstrap'
 import Billing from '../order/Billing'
 import Marketing from '../order/Marketing'
 import OrderInfo from '../order/OrderInfo'
@@ -21,18 +23,31 @@ const PayField = ({input}) => (
     </FormControl>
 )
 
+const getlsNo = (orderId)=> {
+    function pad(num, n) {
+        var len = num.toString().length;
+        while (len < n) {
+            num = "0" + num;
+            len++;
+        }
+        return num;
+    }
+
+    return orderId + pad(Math.floor(Math.random() * 100), 3)
+}
+
 const PayForm = (props) => {
-    const {handleSubmit, onSubmit, onCancel, orderIds, order, marketing, index} = props
+    const {handleSubmit, onSubmit, onCancel, orderIds, order, marketing, index} = props;
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <div className={"marginOutTopBottom"}>
                 <div className="marginBottom">
-                    <span className="marginRight"><b>订单号：</b></span>
-                    <span><b>流水号：</b></span>
+                    <span className="marginRight"><b>订单号：{orderIds[index]}</b></span>
+                    <span><b>流水号：{getlsNo(orderIds[index])}</b></span>
                 </div>
                 <div>
                     <OrderInfo items={order[orderIds[index]].items}/>
-                    <Marketing marketing={marketing[orderIds[index]]} />
+                    <Marketing marketing={marketing[orderIds[index]]}/>
                     <Billing items={order[orderIds[index]].items} marketing={marketing[orderIds[index]]}/>
                 </div>
             </div>
