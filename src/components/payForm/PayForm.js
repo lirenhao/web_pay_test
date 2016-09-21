cf6ad09bcf020de1c7d01747b46eff48f6d2acdb
YuKaiyang <45544205@qq.com>
2016-09-21 16:40:49 +0800

    取消orderIds的排序， 修改用户正在支付中的订单被商户取消时的bug

--- src/components/payForm/PayForm.js
+++ src/components/payForm/PayForm.js
@@ -10,71 +10,76 @@ import React from 'react'
 import {Field, reduxForm} from "redux-form"
 import {ButtonGroup, Button, FormControl, Navbar, Form} from 'react-bootstrap'
 import Billing from '../order/Billing'
-import Marketing from '../order/Marketing'
 import OrderInfo from '../order/OrderInfo'
 import './PayForm.scss'
 
 const PayField = ({input}) => (
-    <FormControl {...input} componentClass="select">
-        <option value="0" checked="checked">成功</option>
-        <option value="1">失败</option>
-    </FormControl>
-)
+	<FormControl {...input} componentClass="select">
+		<option value="0" checked="checked">成功</option>
+		<option value="1">失败</option>
+	</FormControl>
+);
 
 const getlsNo = (orderId)=> {
-    function pad(num, n) {
-        var len = num.toString().length;
-        while (len < n) {
-            num = "0" + num;
-            len++;
-        }
-        return num;
-    }
+	function pad(num, n) {
+		var len = num.toString().length;
+		while (len < n) {
+			num = "0" + num;
+			len++;
+		}
+		return num;
+	}
 
-    return orderId + pad(Math.floor(Math.random() * 100), 3)
-}
+	return orderId + pad(Math.floor(Math.random() * 100), 3)
+};
 
-const PayForm = (props) => {
-    const {handleSubmit, onSubmit, onCancel, orderIds, order, marketing, index} = props;
-    return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <div className={"marginOutTopBottom"}>
-                <div className="marginBottom">
-                    <span className="marginRight"><b>订单号：{orderIds[index]}</b></span>
-                    <span><b>流水号：{getlsNo(orderIds[index])}</b></span>
-                </div>
-                <div>
-                    <OrderInfo items={order[orderIds[index]].items}/>
-                    <Marketing marketing={marketing[orderIds[index]]}/>
-                    <Billing items={order[orderIds[index]].items} marketing={marketing[orderIds[index]]}/>
-                </div>
-            </div>
-            <Navbar fixedBottom>
-                <ButtonGroup justified>
-                    <ButtonGroup>
-                        <Field name="state" component={PayField}/>
-                    </ButtonGroup>
-                    <ButtonGroup>
-                        <Button bsStyle="success" type="submit">确定</Button>
-                    </ButtonGroup>
-                    <ButtonGroup>
-                        <Button bsStyle="danger" onClick={() => onCancel(orderIds[index])}>取消支付</Button>
-                    </ButtonGroup>
-                </ButtonGroup>
-            </Navbar>
-        </Form>
-    )
+class PayForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {items: this.props.order[this.props.orderIds[this.props.index]].items}
+	}
+
+	render() {
+		const {handleSubmit, onSubmit, onCancel, orderIds, marketing, index} = this.props;
+		return (
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<div className={"marginOutTopBottom"}>
+					<div className="marginBottom">
+						<span className="marginRight"><b>订单号：{orderIds[index]}</b></span>
+						<span><b>流水号：{getlsNo(orderIds[index])}</b></span>
+					</div>
+					<div>
+						<OrderInfo items={this.state.items}/>
+						<Billing items={this.state.items} marketing={marketing[orderIds[index]] || ""}/>
+					</div>
+				</div>
+				<Navbar fixedBottom>
+					<ButtonGroup justified>
+						<ButtonGroup>
+							<Field name="state" component={PayField}/>
+						</ButtonGroup>
+						<ButtonGroup>
+							<Button bsStyle="success" type="submit">确定</Button>
+						</ButtonGroup>
+						<ButtonGroup>
+							<Button bsStyle="danger" onClick={() => onCancel(orderIds[index])}>取消支付</Button>
+						</ButtonGroup>
+					</ButtonGroup>
+				</Navbar>
+			</Form>
+		)
+	}
 }
 
 PayForm.propTypes = {
-    onSubmit: React.PropTypes.func.isRequired,
-    onCancel: React.PropTypes.func.isRequired,
-    orderIds: React.PropTypes.array.isRequired,
-    order: React.PropTypes.object.isRequired,
-    marketing: React.PropTypes.object.isRequired,
-    index: React.PropTypes.number.isRequired
-}
+	onSubmit: React.PropTypes.func.isRequired,
+	onCancel: React.PropTypes.func.isRequired,
+	orderIds: React.PropTypes.array.isRequired,
+	order: React.PropTypes.object.isRequired,
+	marketing: React.PropTypes.object.isRequired,
+	index: React.PropTypes.number.isRequired
+};
 
 export default reduxForm({
-    form: "payForm"
+	form: "payForm"
 })(PayForm)
