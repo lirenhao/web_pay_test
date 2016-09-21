2fef1f836ac18965cb5d03331d7c28ac7ff325b6
jpf0318 <pengfei.ji@bjyada.com>
2016-09-21 16:42:32 +0800

    修改商品信息组件部分测试内容

--- test/components/order/GoodsItem-test.js
+++ test/components/order/GoodsItem-test.js
@@ -10,7 +10,6 @@ import GoodsItem from '../../../src/components/order/GoodsItem'
 import React from 'react';
 import {expect} from 'chai';
 import {shallow, mount, render} from 'enzyme';
-import AmtDisplay from '../../../src/components/order/AmtDisplay'
 
 
 describe("测试商品信息展示组件（GoodsItem）", function () {
@@ -19,12 +18,12 @@ describe("测试商品信息展示组件（GoodsItem）", function () {
         data = {name: "哈哈", price: 200, quantity: 2};
     });
 
-    it("测试组件元素是否包含价格组件(AmtDisplay)", function () {
-        expect(shallow(<GoodsItem/>).find('AmtDisplay')).to.have.length(1);
+    it("测试组件元素是渲染出数据个数是否正确", function () {
+        expect(shallow(<GoodsItem/>).find('td')).to.have.length(3);
     });
 
-    it("测试组件元素包含价格组件(AmtDisplay)是否显示正确属性值", function () {
-        expect(shallow(<GoodsItem {...data}/>).contains(<AmtDisplay amt={data.price}/>)).to.equal(true);
+    it("测试组件元素包含价格组件(AmtDisplay)是否渲染", function () {
+        expect(mount(<GoodsItem {...data}/>).find('span')).to.have.length(1);
     });
 
     it("测试组件props的name、price、quantity是否都获取到相应数据", function () {
