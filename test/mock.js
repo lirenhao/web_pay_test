6b8998cabc37d48c76a2da8f0ceb0a7c1d4f33e6
liRenhao <renhao.li@bjyada.com>
2016-09-25 12:29:55 +0800

    MockServer调整

--- test/mock.js
+++ test/mock.js
@@ -6,92 +6,43 @@
  * <文件描述>
  */
 import {Server} from 'mock-socket'
-import Const from '../src/constants'
-
-const ServerCmd = Const.ServerCmd
 
 export class MockServer {
-    constructor(order = {}, marketing = {}) {
+    constructor() {
         this.server = new Server(process.env.wsUrl)
-        this.server.on('message', data => {
-            const {eventType, ...msg} = JSON.parse(data)
-            switch (eventType) {
-                case ServerCmd.CLIENT_SIGN_IN:
-                    // 用户登录
-                    this.server.send(JSON.stringify({"eventType":"ORDER_ITEMS","orderId":"11","items":[{"name":"ONLY修身撞色拼接女针织裙","price":34950,"quantity":2},{"name":"ONLY圆点荷叶边女修身裙","price":19950,"quantity":1},{"name":"ONLY棉宽松字母牛仔女外套","price":27450,"quantity":1}]}))
-                    this.clientSignIn(order)
-                    break
-                case ServerCmd.CREATE_ORDER:
-                    // 创建订单
-                    this.createOrder(msg.items)
-                    break
-                case ServerCmd.JOIN_ORDER:
-                    // 关联订单
-                    this.joinOrder()
-                    break
-                case ServerCmd.CANCEL_ORDER:
-                    // 取消订单
-                    this.cancelOrder()
-                    break
-                case ServerCmd.PAY_AUTH_REQ:
-                    // 请求支付权限
-                    this.payAuthReq()
-                    break
-                case ServerCmd.GIVE_UP_PAY:
-                    // 取消支付权限
-                    this.giveUpPay()
-                    break
-                case ServerCmd.PAY_RESULT:
-                    // 发送支付结果
-                    this.payResult()
-                    break
-            }
-        })
     }
 
-    clientSignIn(order) {
-        if (Object.keys(order).length > 0) {
-            this.server.send("test")
-        }
+    getClientDate() {
+        return this.clientDate
     }
 
-    createOrder(goods) {
-        this.server.send({
-            eventType: ServerCmd.CREATE_ORDER,
-            id: user.userId,
-            terminalType: user.userType,
-            items: goods
-        })
+    sendOrder(order) {
+        this.server.send(JSON.stringify(order))
     }
 
-    joinOrder(user, orderId) {
-        this.server.send({
-            eventType: ServerCmd.JOIN_ORDER,
-            id: user.userId,
-            terminalType: user.userType,
-            orderId: orderId
-        })
+    sendMarketing(marketing) {
+        this.server.send(JSON.stringify(marketing))
     }
 
-    cancelOrder() {
-        this.server.send({})
+    sendPayAuth() {
+        this.server.send(JSON.stringify(order))
     }
 
-    payAuthReq() {
-        this.server.send({})
+    sendPayCompleted(order) {
+        this.server.send(JSON.stringify(order))
     }
 
-    giveUpPay() {
-        this.server.send({})
+    sendFail() {
+        this.server.send(JSON.stringify(order))
     }
 
-    payResult() {
-        this.server.send({})
+    sendMessage() {
+        this.server.send(JSON.stringify(order))
     }
 }
 
 export class MockHistory {
-    constructor(){
+    constructor() {
         this.routes = []
     }
 
