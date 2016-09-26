f31b9b0581db7e93159aa666cc042bac0032ca39
kongli860329@163.com <kongli860329@163.com>
2016-09-26 12:05:11 +0800

    添加扫描订单页面的注释

--- src/containers/OrderId.js
+++ src/containers/OrderId.js
@@ -4,6 +4,8 @@
  * Modified By：liRenhao
  * Why & What is modified  <修改原因描述>
  * 添加订单号表单的容器
+ * Modified By：kongli
+ * Why & What is modified 添加注释
  */
 
 import React from "react"
@@ -11,9 +13,19 @@ import {connect} from "react-redux"
 import Payment from "../Payment"
 import OrderIdForm from "../components/orderIdForm/OrderIdForm"
 
+/**
+ * 定义扫描订单页面容器
+ * @param props 组件属性
+ * @param context 上下文属性
+ */
 const OrderId = (props, context) => {
 	context.setTitle("扫描订单")
+	//定义组件的属性
 	const {user, orderIds} = props
+	/**
+	 * 匹配订单事件
+	 * @param values
+     */
 	const onSubmit = (values) => {
 		// 判断state中是否有该订单
 		const index = orderIds.indexOf(values.orderId)
@@ -25,6 +37,9 @@ const OrderId = (props, context) => {
 			context.router.push("/order")
 		}
 	}
+	/**
+	 * 待支付按钮点击事件
+     */
 	const onButton = () => {
 		context.router.push("/order")
 	}
@@ -33,14 +48,30 @@ const OrderId = (props, context) => {
 	)
 }
 
+/**
+ * 扫描订单页面必须传的属性
+ * @type {{setTitle: *}} 页面标题
+ * @type {{router: *}}   路由
+ */
 OrderId.contextTypes = {
 	setTitle: React.PropTypes.func.isRequired,
 	router: React.PropTypes.object.isRequired
 }
 
+/**
+ *  该函数作为connect的参数
+ * 定义该参数，支付组件将会监听 Redux store 的变化。
+ * 该回调函数必须返回一个纯对象，这个对象会与组件的 props 合并。
+ * @param state 组件状态
+ */
 const mapStateToProps = (state)=> ({
 	user: state.user,
 	orderIds: state.orderIds
 })
 
+/**
+ *  输出react-redux关联之后的支付容器组件
+ * 连接支付组件与 Redux store
+ * 只要 Redux store 发生改变，mapStateToProps 函数就会被调用。
+ */
 export default connect(mapStateToProps)(OrderId)
