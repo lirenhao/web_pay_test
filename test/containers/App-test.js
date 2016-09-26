c260d8941fd2612d44edbfa51f9d8c472ca02edd
jpf0318 <pengfei.ji@bjyada.com>
2016-09-26 13:38:42 +0800

    优化测试app容器代码

--- test/containers/App-test.js
+++ test/containers/App-test.js
@@ -3,6 +3,8 @@
  * Create Date：2016/9/21
  * Modified By：liRenhao
  * Why & What is modified  <修改原因描述>
+ * Modified By：Pengfei
+ * Why & What is modified  优化测试代码
  * <文件描述>
  */
 import React from "react"
@@ -16,7 +18,9 @@ describe('AppContainer', () => {
     it('should render', () => {
         const expectedState = {dialog: {}}
         const component = shallowWithState(<App />, expectedState)
-        expect(component.props().dialog).to.equal(expectedState.dialog)
+        expect(component.props().dialog.show).to.equal(false)
+        expect(component.props().dialog.header).to.equal('')
+        expect(component.props().dialog.body).to.equal('')
         expect(component.props().close).to.a('function');
     })
 })
