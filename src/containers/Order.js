0aae54ebace8b9c7c69033fe5912353782e33a96
kongli860329@163.com <kongli860329@163.com>
2016-09-26 12:00:26 +0800

    添加订单页面的注释

--- src/containers/Order.js
+++ src/containers/Order.js
@@ -13,12 +13,26 @@ import Payment from "../Payment"
 import Const from "../constants"
 import OrderShow from "../components/order/Order"
 
+/**
+ * 定义订单信息页面容器
+ * @param props 组件属性
+ * @param context 上下文属性
+ */
 const Order = (props, context) => {
 	context.setTitle("订单");
+	//定义组件的属性
 	const {user, orderIds, order, marketing} = props;
+	/**
+	 * 定义取消订单事件
+	 * @param orderId 订单号
+     */
 	const onCancel = (orderId) => {
 		Payment.cancelOrder(user, orderId)
 	};
+	/**
+	 * 支付请求事件
+	 * @param orderId  订单号
+     */
 	const onReqPay = (orderId) => {
 		Payment.reqPayAuth(user, orderId)
 	};
@@ -30,8 +44,18 @@ const Order = (props, context) => {
 	                  canCancel={user.userType == Const.TerminalType.MERCHANT}/>
 };
 
+/**
+ * 订单页面必传的属性
+ * @type {{setTitle: *}} 标题内容
+ */
 Order.contextTypes = {setTitle: React.PropTypes.func.isRequired};
 
+/**
+ *  该函数作为connect的参数
+ * 定义该参数，支付组件将会监听 Redux store 的变化。
+ * 该回调函数必须返回一个纯对象，这个对象会与组件的 props 合并。
+ * @param state 组件的状态
+ */
 const mapStateToProps = (state)=> ({
 	user: state.user,
 	orderIds: state.orderIds,
@@ -39,4 +63,9 @@ const mapStateToProps = (state)=> ({
 	marketing: state.marketing
 });
 
+/**
+ *  输出react-redux关联之后的支付容器组件
+ * 连接支付组件与 Redux store
+ * 只要 Redux store 发生改变，mapStateToProps 函数就会被调用。
+ */
 export default connect(mapStateToProps)(Order)
